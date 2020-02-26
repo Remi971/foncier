7278f565b7cd5b8cdd239a92c6579a37324d8f00
Remi <rbhagalou@citadia.com>
2020-02-26 12:43:54 +0100

    Added some colors and image data selection needs to be fixed

--- interface eel/interface/js/script4.js
+++ interface eel/interface/js/script4.js
@@ -51,7 +51,7 @@ async function pickGpkg(){
 $(document).ready(function(){
   $("#btn-addFilter").on('click', function(){
     let name = prompt("Indiquez le nom du filtre : ")
-    $('<div class="group"><button class="btn-test" id='+name+'>'+name+'</button><span id="vf-canvas" class="data-info"></span><button class="remove">X</button></div>').appendTo('.filtres');
+    $('<div class="group"><button style= "background-color: #ff8f00"  class="btn-test" id='+name+'>'+name+'</button><button class="remove">X</button><span id="vf-canvas" class="data-info"></span></div>').appendTo('.btn-base');
     $(".group").on('click',".btn-test", function(){
       let select = $(".classLi").html();
       let divParent = $(this).parent();
