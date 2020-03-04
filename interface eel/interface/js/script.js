5d926e3228444664296579975047a49a9a785570
Remi <rbhagalou@citadia.com>
2020-03-04 14:03:34 +0100

    ajout de l'onglet visualisation et d'une map

--- interface eel/interface/js/script.js
+++ interface eel/interface/js/script.js
@@ -178,3 +178,20 @@ $(document).ready(function(){
     valeursTable(listeValeurs);
   })
 })
+
+//VISUALISATION map
+let mymap = L.map('mapid').setView([43.980981, 4.741116], 13);
+let Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+let OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+OpenStreetMap_Mapnik.addTo(mymap);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
